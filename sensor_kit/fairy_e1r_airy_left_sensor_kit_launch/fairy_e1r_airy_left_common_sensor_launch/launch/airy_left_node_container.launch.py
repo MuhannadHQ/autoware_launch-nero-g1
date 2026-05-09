d62f19f6fbba7c0f2eb41d5a77c940d5f2cea0a9
Sameer Bin Khalid <sameerbkhalid@gmail.com>
2026-05-09 17:26:24 +0300

    Update calibration parameters

--- sensor_kit/fairy_e1r_airy_left_sensor_kit_launch/fairy_e1r_airy_left_common_sensor_launch/launch/airy_left_node_container.launch.py
+++ sensor_kit/fairy_e1r_airy_left_sensor_kit_launch/fairy_e1r_airy_left_common_sensor_launch/launch/airy_left_node_container.launch.py
@@ -28,8 +28,8 @@ def _bool(value: str) -> bool:
 
 
 def launch_setup(context, *args, **kwargs):
-    pkg_dir = get_package_share_directory("fairy_e1r_airy_left_right_common_sensor_launch")
-    cropbox_param_file = os.path.join(pkg_dir, "config", "crop_box_filter_self_left.param.yaml")
+    pkg_dir = get_package_share_directory("fairy_e1r_airy_left_common_sensor_launch")
+    cropbox_param_file = os.path.join(pkg_dir, "config", "crop_box_filter_self.param.yaml")
 
     container_name = LaunchConfiguration("container_name").perform(context)
     node_name = LaunchConfiguration("node_name").perform(context)
